2f1809586dd327328908836533719987f49948ec
Gonzalo Ibarz Barbera <gibarz@vectoritcgroup.com>
2016-10-11 17:07:03 +0200

    Added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gutil= require('gulp-util');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
 
 var jsSources = [
 	'components/scripts/rclick.js',
@@ -19,5 +20,6 @@ gulp.task('coffee', function(){
 gulp.task('js', function(){
 	gulp.src(jsSources)
 	.pipe(concat('script.js'))
+	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
 });
